096a96ed778cc2aec10a5fa7b10c49cc84f28af0
Jan Kornienko <kor.jan@outlook.com>
2021-03-16 21:11:48 +0100

    Working Jquery

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>Page</h1>
+  <Page msg="Gallery & To Do"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import Page from './components/Page.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'App',
+		components: {
+			Page
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
---
+++ src/components/Page.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="container">
+		<div class="box">
+			{{ msg }}
+		</div>
+	</div>
+</template>
+
+<script>
+	import $ from 'jquery';
+
+	export default {
+		name: 'Page',
+		props: {
+			msg: String
+		},
+		data() {
+			return {
+				url: 'https://jsonplaceholder.typicode.com',
+				galleryUrl: '/albums/1/photos',
+				todoUrl: '/users/1/todos',
+				galleryObject: {},
+				todoObject: {}
+			};
+		},
+		created() {
+			$.get(this.url + this.galleryUrl, (response) => {
+				this.galleryObject = response;
+				console.log(response);
+			})
+		}
+	};
+</script>
+
+<style scoped>
+</style>
